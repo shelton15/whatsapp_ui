0ae84d63f5d7cafddb683b603e86e0cd0a8ec1f3
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-09 09:02:53 +0100

    tabBar added

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('This is mobile view')),
-    );
+    return DefaultTabController(
+      length: 3, 
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('WhatsApp', style: TextStyle(
+            color: Colors.grey,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),),
+          centerTitle: false,
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search, color: Colors.grey,),),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert, color: Colors.grey,),),
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              
+            ),
+            tabs: [
+            Tab(text: 'CHATS',),
+            Tab(text: 'STATUS',),
+            Tab(text: 'CALLS',),
+          ])
+        ),
+      ),
+      );
   }
 }
