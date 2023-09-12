526ffe25a92ec55da80245724f9e9f196d29e2dd
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-12 22:52:27 +0100

    wrapped listview builder with column

--- lib/widgets/contacts_lists.dart
+++ lib/widgets/contacts_lists.dart
@@ -8,34 +8,38 @@ class ContactsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: ListView.builder(
-        itemCount: info.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: Text(
-                info[index]['name'].toString(),
-                style: const TextStyle(fontSize: 18),),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 6),
-                child: Text(
-                  info[index]['message'].toString(),
-                  style: const TextStyle(fontSize: 15),),
-              ),
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  info[index]['profilePic'].toString()
+      child: Column(
+        children: [
+          ListView.builder(
+            itemCount: info.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text(
+                    info[index]['name'].toString(),
+                    style: const TextStyle(fontSize: 18),),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 6),
+                    child: Text(
+                      info[index]['message'].toString(),
+                      style: const TextStyle(fontSize: 15),),
+                  ),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      info[index]['profilePic'].toString()
+                    ),
+                  ),
+                  trailing: Text(
+                    info[index]['time'].toString(),
+                    style: const TextStyle(fontSize: 13, color: Colors.grey,),
+                  ),
                 ),
-              ),
-              trailing: Text(
-                info[index]['time'].toString(),
-                style: const TextStyle(fontSize: 13, color: Colors.grey,),
-              ),
+              );
+            }
             ),
-          );
-        }
-        ),
+        ],
+      ),
       );
   } 
 }
