08a0dacc40f01d69f60eec931bfafacfef13a55c
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-12 14:32:29 +0100

    contact list added with static objects

---
+++ lib/widgets/contacts_lists.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  const ContactsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
