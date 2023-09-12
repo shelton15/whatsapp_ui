8ed6377a317a05b194c495d018bb42d066d41d36
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-12 22:59:48 +0100

    wrap list view in expanded

--- lib/widgets/contacts_lists.dart
+++ lib/widgets/contacts_lists.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
 import 'package:whatsapp_ui/info.dart';
 
 class ContactsList extends StatelessWidget {
@@ -10,34 +11,37 @@ class ContactsList extends StatelessWidget {
       padding: const EdgeInsets.only(top: 10),
       child: Column(
         children: [
-          ListView.builder(
-            itemCount: info.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  title: Text(
-                    info[index]['name'].toString(),
-                    style: const TextStyle(fontSize: 18),),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.only(top: 6),
-                    child: Text(
-                      info[index]['message'].toString(),
-                      style: const TextStyle(fontSize: 15),),
-                  ),
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      info[index]['profilePic'].toString()
+          Expanded(
+            child: ListView.builder(
+              itemCount: info.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(fontSize: 18),),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: const TextStyle(fontSize: 15),),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        info[index]['profilePic'].toString()
+                      ),
+                    ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(fontSize: 13, color: Colors.grey,),
                     ),
                   ),
-                  trailing: Text(
-                    info[index]['time'].toString(),
-                    style: const TextStyle(fontSize: 13, color: Colors.grey,),
-                  ),
-                ),
-              );
-            }
-            ),
+                );
+              }
+              ),
+          ),
+            const Divider(color: dividerColor,)
         ],
       ),
       );
