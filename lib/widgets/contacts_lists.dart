e216532c9d3ab38ef1f10ef51e3ebfed61710163
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-12 22:06:58 +0100

    added const

--- lib/widgets/contacts_lists.dart
+++ lib/widgets/contacts_lists.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/info.dart';
 
 class ContactsList extends StatelessWidget {
   const ContactsList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(top: 10),
-    );
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: ListView.builder(
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(info[index]['name'].toString(),style: const TextStyle(fontSize: 18),),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 6),
+              child: Text(info[index]['message'].toString(),style: const TextStyle(fontSize: 15),),
+            ),
+          );
+        }
+        ),
+      );
   }
 }
