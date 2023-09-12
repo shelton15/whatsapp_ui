42718f0f36a9c89d93fb7ee0f3e941f23323d59f
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-12 23:03:35 +0100

    wrap in inkwell widget

--- lib/widgets/contacts_lists.dart
+++ lib/widgets/contacts_lists.dart
@@ -16,26 +16,28 @@ class ContactsList extends StatelessWidget {
               shrinkWrap: true,
               itemCount: info.length,
               itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    title: Text(
-                      info[index]['name'].toString(),
-                      style: const TextStyle(fontSize: 18),),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 6),
-                      child: Text(
-                        info[index]['message'].toString(),
-                        style: const TextStyle(fontSize: 15),),
-                    ),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        info[index]['profilePic'].toString()
+                return InkWell(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(
+                        info[index]['name'].toString(),
+                        style: const TextStyle(fontSize: 18),),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 6),
+                        child: Text(
+                          info[index]['message'].toString(),
+                          style: const TextStyle(fontSize: 15),),
                       ),
-                    ),
-                    trailing: Text(
-                      info[index]['time'].toString(),
-                      style: const TextStyle(fontSize: 13, color: Colors.grey,),
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          info[index]['profilePic'].toString()
+                        ),
+                      ),
+                      trailing: Text(
+                        info[index]['time'].toString(),
+                        style: const TextStyle(fontSize: 13, color: Colors.grey,),
+                      ),
                     ),
                   ),
                 );
